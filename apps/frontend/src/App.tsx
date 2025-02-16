65060e9a8dbd285807681be75e145a6149ef4f86
ruslan_kuchmynda <ruslankuchmynda@gmail.com>
2025-02-16 17:53:06 +0200

    fix-pre-commit

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,7 +1,5 @@
-function App()     {
-  return (
-      <div className="flex justify-center">test</div>
-  );
+function App() {
+  return <div className="flex justify-center">test</div>;
 }
 
 export default App;
