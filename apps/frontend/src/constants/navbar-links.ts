0baaef75a9b78a1712ef5977c6a2d1f0d7f047b3
ruslan_kuchmynda <ruslankuchmynda@gmail.com>
2025-02-17 00:55:02 +0200

    Add demo navbar

---
+++ apps/frontend/src/constants/navbar-links.ts
@@ -0,0 +1,8 @@
+import { paths } from "@/constants/paths";
+
+export const navbarLinks = [
+  { link: paths.problems, label: "Problems" },
+  { link: paths.leaderboard, label: "Leaderboard" },
+  { link: paths.about, label: "About us" },
+  { link: paths.profile, label: "Profile" },
+];
---
+++ apps/frontend/src/constants/paths.ts
@@ -0,0 +1,9 @@
+export const paths = {
+  home: "/",
+  problems: "/problems",
+  leaderboard: "/leaderboard",
+  about: "/about",
+  profile: "/profile",
+  signIn: "/sign-in",
+  signUp: "/sign-up",
+};
