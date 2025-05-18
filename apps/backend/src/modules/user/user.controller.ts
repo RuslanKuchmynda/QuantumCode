61c2c42e364747c14ca4760049559e466902e627
Ruslan Kuchmynda <ruslankuchmynda@gmail.com>
2025-05-18 15:54:00 +0300

    fix

--- apps/backend/src/modules/user/user.controller.ts
+++ apps/backend/src/modules/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, Param, UseGuards } from "@nestjs/common";
+import { Controller, Get, Req, UseGuards } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { JwtAuthGuard } from "../auth/jwt-auth.guard";
 import { Request } from "express";
